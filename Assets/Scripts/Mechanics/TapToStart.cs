5322884bfdc7757212d5bfac76a2c4327c46d447
CaelusKen <caelus.khangthuan1209@gmail.com>
2024-10-22 15:03:53 +0700

    Push project to Github

---
+++ Assets/Scripts/Mechanics/TapToStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class TapToStart : MonoBehaviour
+{
+    CanvasElementVisibility visibility;
+    void Start()
+    {
+        visibility = GetComponent<CanvasElementVisibility>();
+        GameController.Instance.GameStarted.Where((value) => value).Subscribe((value) =>
+        {
+            visibility.Visible = false;
+        }).AddTo(this);
+    }
+}
